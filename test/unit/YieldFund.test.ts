b1a71de002f081cfc0e3f4d211389eb7fed449b8
Silas Lenihan <sjl58@duke.edu>
2022-09-12 12:14:51 -0400

    cleaned up comments

--- test/unit/YieldFund.test.ts
+++ test/unit/YieldFund.test.ts
@@ -138,14 +138,6 @@ import * as fs from "fs"
                       "YieldFund__FundAmountMustBeAboveZero"
                   )
               })
-
-              //   it("doesn't allow a funder to withdraw more than they funded", async function () {
-              //       yieldFund = yieldFundContract.connect(user)
-              //       const higherFundAmount = fundAmount.add(1)
-              //       await expect(
-              //           yieldFund.withdrawFundsFromPool(higherFundAmount)
-              //       ).to.be.revertedWith("YieldFund__WithdrawFundsGreaterThanBalance")
-              //   })
           })
           describe("Owner Tests", function () {
               it("fails when a non owner tries to withdraw proceeds", async function () {
